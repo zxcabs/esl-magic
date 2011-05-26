0a5c79a5789f9d80802d5bba6a97fbe5e54a1386
Evgeny Reznichenko <kusakykyr@gmail.com>
2011-05-26 23:54:17 +0400

    index.html: a href became a little less add: joader.js -> getContainer(), include(), loader()

--- www/js/loader.js
+++ www/js/loader.js
@@ -1,3 +1,55 @@
 (function () {
-	alert('loader!');
+	var container = null;
+	var jQuery = window.jQuery;
+	var rz = window._rz = window._rz || {};
+	
+	function getContainer () {
+		if (!container) {
+			container = document.getElementById('rzct');
+			
+			if (!container) {
+				container = document.createElement('div');
+				container.id = 'rz' + Math.random();
+				document.body.appendChild(container);
+			};
+		};
+		
+		return container;
+	};
+	
+	//include file on page
+	function include(url) {
+		if (!url && typeof(url) != "string" && url.match(/\.\w+$/i)) return false;
+		var type = url.match(/\.\w+$/i)[0];
+		var el;
+		
+		switch (type) {
+			case '.js':
+				el = document.createElement('script');
+				el.type='text/javascript';
+				el.charset='utf8';
+				el.src=url;
+				getContainer().appendChild(el);
+				break;
+			case '.css':
+				//TODO: need do?
+				break;
+			default:
+				return false;
+		};
+		
+		return true;
+	};
+	
+	function loader () {
+		var host = window.location.host;
+		if (!host && !host.match(/esl\.eu$/gi)) return false;
+
+		if (!jQuery || (jQuery && jQuery.fn && parseFloat(jQuery.fn.jquery) < 1.5)) {
+			include('http://code.jquery.com/jquery-1.6.1.min.js');
+		};
+
+	};
+	
+	loader();
 })();
