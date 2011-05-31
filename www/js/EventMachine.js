c102fbd12d3809d29cff1d451da9bfcb504f63a8
Evgeny Reznichenko <kusakyky@gmail.com>
2011-05-31 13:24:13 +0400

    add: EventMachine.js  - skeleton for pub/sub method

--- www/js/EventMachine.js
+++ www/js/EventMachine.js
@@ -3,7 +3,18 @@
 	var rz = window._rz_;
 	if (!rz || rz.EventMachine) return false;
 	
-	function EventMachine () {};
-
+	function EventMachine (opt) {
+		this._opt = opt || {};
+	};
+	
+	//Publish msg
+	EventMachine.prototype.pub = function (name, data) {
+	};
+	
+	//Subscribe
+	EventMachine.prototype.sub = function (name, callback) {
+	};
+	
+	rz.EventMachine = EventMachine;
 	rz.scriptLoaded('EventMachine');
 })();
