3d477ae3cb1104dc2e3cfcc5b5dcbc5e54c37514
Evgeny Reznichenko <kusakykyr@gmail.com>
2011-06-01 23:38:02 +0400

    index.html  update:   - change title
    DataProvider.js
     add:
      - parseESLTable

--- www/js/DataProvider.js
+++ www/js/DataProvider.js
@@ -14,6 +14,8 @@
 		this._opt = opt;
 		this._core = this._opt.core;
 		
+		this._data = {};
+		
 		if(this['_getData' + this._opt.type] && this['_parseData' + this._opt.type]) {
 			this._getData = this['_getData' + this._opt.type];
 			this._parseData = this['_parseData' + this._opt.type];
@@ -40,10 +42,10 @@
 		
 		$.ajax({
 				url: this._core.path,
-				type: 'xml'
+				type: 'html'
 			}
 		).done(function (data) {
-				this._core.emit('endLoad', data)
+				this._core.emit('endLoad', data.replace(/<!DOCTYPE.*>|\n{1,}|\r|\s{2,}|\t{1,}/gi, ''))
 			}.bind(this)
 		).fail(function () {
 			this.error('load faild');
@@ -53,7 +55,47 @@
 	
 	DataProvider.prototype._parseDataESL = function (data) {
 		this.log('parseDataESL');
-		this._core.emit('endParse');
+		
+		var $main = $('#main_content', $(data));
+		var $tables = $('table', $main);
+		
+		if ($tables && $tables[0] && $tables[1]) {
+			var t0 = $($tables[0]),
+				t1 = $($tables[1]);
+			
+			this._parseESLTable('t0', t0, function () {
+				this._parseESLTable('t1', t1, function () {
+					this._core.emit('endParse');
+				}.bind(this));
+			}.bind(this));
+		} else {
+			this.error('no parse data');
+			this._core.emit('endParse');
+		};
+	};
+	
+	DataProvider.prototype._parseESLTable = function (name, $tbl, callback) {
+		this.log('parse ESL table: ' + name);
+		
+		if ($tbl) {
+			
+			var $trs = $('tr', $tbl);
+			var rcount = $('td.TextS', $trs[0]).length;
+			
+			//TODO: async parse
+			for (var i = 2; i < $trs.length; i++) {
+				var $tds = $('td', $trs[i]);
+				var r = rcount - (parseInt($($tds[$tds.length - 1]).attr('colspan')) / 2)  + 1;
+				//TODO: parse
+				r;
+			}
+			
+		} else {
+			this.error('table ' + name + ' is null');
+		};
+		
+		if (callback) setTimeout(callback, 0);
+		return;
 	};
 	
 	DataProvider.prototype.log = function (msg) {rz.log('DataProvider: ' + msg)};
