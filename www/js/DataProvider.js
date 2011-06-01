d3870749b08728f5a96c3529eed9c96ad21bec1f
Evgeny Reznichenko <kusakyky@gmail.com>
2011-06-01 13:51:42 +0400

    add: DataProvider.js  - parser() method

--- www/js/DataProvider.js
+++ www/js/DataProvider.js
@@ -12,13 +12,14 @@
 		this._opt = opt;
 		this._core = this._opt.core;
 		
-		if(this['_getData' + this._opt.type]) {
+		if(this['_getData' + this._opt.type] && this['_parseData' + this._opt.type]) {
 			this._getData = this['_getData' + this._opt.type];
+			this._parseData = this['_parseData' + this._opt.type];
 		} else {
 			this.error('Wrong type!');
 		};
 		
-		this._core.on('getData', this._getData.bind(this))
+		this._core.on('getData', this._getData.bind(this));
 		
 		this._ready();
 	};
@@ -29,10 +30,16 @@
 	};
 	
 	DataProvider.prototype._getData = function () {this.error('getData(),  doesn\'t select!')};
+	DataProvider.prototype._parseData = function () {this.error('_parseData(),  doesn\'t select!')};
+	
 	DataProvider.prototype._getDataESL = function () {
 		this.log('get esl data');
+		this._core.emit('loadData');
 	};
 	
+	DataProvider.prototype._parseDataESL = function () {
+		this.log('parseDataESL');
+	};
 	
 	DataProvider.prototype.log = function (msg) {rz.log('DataProvider: ' + msg)};
 	DataProvider.prototype.error = function (msg) {rz.error('DataProvider error: ' + msg)};
